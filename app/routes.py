8e613f0eab3f211da22a4fe54b4c206d74d6d626
dsheah002 <85600959+dsheah002@users.noreply.github.com>
2022-05-12 17:33:06 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 from app import app
 from flask import render_template
 =======
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
->>>>>>> 361502675e92c26d5b2c72f509de4b86eeb90d91
+
 
 
 @app.route('/')
